8bda4ad23f0b59ad062f83146d03e24f2cab56b9
gpaleta <128159891+gpaleta@users.noreply.github.com>
2024-10-15 09:25:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var container = document.getElementById("tudo")
+var title = document.getElementById("title")
+function play(){
+    container.innerHTML = ""
+    container.innerHTML = "<header id='title'><h1>Memória da Turminha</h1></header>"
+}
