0073f26b3d8b1cda9033389b164e6fcb4966a1cb
gpaleta <128159891+gpaleta@users.noreply.github.com>
2024-10-15 09:53:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ var container = document.getElementById("tudo")
 var title = document.getElementById("title")
 function play(){
     container.innerHTML = ""
-    container.innerHTML = "<header id='title'><h1>Memória da Turminha</h1></header>"
+    container.innerHTML = "<header id='title'><h1>Memória da Turminha</h1></header> <table> <tr> <td align='center'>    <img src='./img/Set_vertical.webp' alt='' class='yuguio' onclick='virar()'> </td> <td align='center'>    <img src='./img/Set_vertical.webp' alt='' class='yuguio' onclick='virar()'> </td> <td align='center'>    <img src='./img/Set_vertical.webp' alt='' class='yuguio' onclick='virar()'> </td> </tr> <tr> <td align='center'>    <img src='./img/Set_vertical.webp' alt='' class='yuguio' onclick='virar()'> </td> <td align='center'>    <img src='./img/Set_vertical.webp' alt='' class='yuguio' onclick='virar()'> </td> <td align='center'>    <img src='./img/Set_vertical.webp' alt='' class='yuguio' onclick='virar()'> </td> </tr> <tr> <td align='center'>    <img src='./img/Set_vertical.webp' alt='' class='yuguio' onclick='virar()'> </td> <td align='center'>   <img src='./img/Set_vertical.webp' alt='' class='yuguio' onclick='virar()'> </td> <td align='center'>   <img src='./img/Set_vertical.webp' alt='' class='yuguio' onclick='virar()'> </td> </tr>"
+}
+function virar(){
+    container.innerHTML = ""
 }
